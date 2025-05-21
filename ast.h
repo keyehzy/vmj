b89f7f2693ebb9bb57bed553f2c93a6b0cfc60ef
Matheus <msousa@sjtu.edu.cn>
2025-05-21 18:18:54 +0800

    map for to while

--- ast.h
+++ ast.h
@@ -11,12 +11,14 @@ struct Ast {
   enum class Type {
     FunctionDeclaration,
     Block,
-    For,
+    While,
     VariableDeclaration,
     LessThan,
     Increment,
     Literal,
     Variable,
+    Assignment,
+    Return,
   };
 
   Type type;
@@ -133,29 +135,45 @@ struct Increment : public Ast {
   void dump(std::ostream &os) const override { os << "Increment(" << variable->name << ")"; }
 };
 
-struct For : public Ast {
-  std::unique_ptr<VariableDeclaration> initializer;
+struct While : public Ast {
   std::unique_ptr<LessThan> condition;
-  std::unique_ptr<Increment> increment;
   std::unique_ptr<Block> body;
 
-  For(std::unique_ptr<VariableDeclaration> initializer, std::unique_ptr<LessThan> condition,
-      std::unique_ptr<Increment> increment, std::unique_ptr<Block> body)
-      : Ast(Type::For),
-        initializer(std::move(initializer)),
-        condition(std::move(condition)),
-        increment(std::move(increment)),
-        body(std::move(body)) {}
+  While(std::unique_ptr<LessThan> condition, std::unique_ptr<Block> body)
+      : Ast(Type::While), condition(std::move(condition)), body(std::move(body)) {}
 
   void dump(std::ostream &os) const override {
-    os << "For(";
-    initializer->dump(os);
-    os << ", ";
+    os << "While(";
     condition->dump(os);
     os << ", ";
-    increment->dump(os);
-    os << ", ";
     body->dump(os);
+    os << ")";
+  }
+};
+
+struct Assignment : public Ast {
+  std::string name;
+  std::unique_ptr<Ast> value;
+
+  Assignment(std::string name, std::unique_ptr<Ast> value)
+      : Ast(Type::Assignment), name(name), value(std::move(value)) {}
+
+  void dump(std::ostream &os) const override {
+    os << "Assignment(" << name << ", ";
+    value->dump(os);
+    os << ")";
+  }
+};
+
+struct Return : public Ast {
+  std::unique_ptr<Ast> value;
+
+  Return(std::unique_ptr<Ast> value) : Ast(Type::Return), value(std::move(value)) {}
+
+  void dump(std::ostream &os) const override {
+    os << "Return(";
+    value->dump(os);
+    os << ")";
   }
 };
 
@@ -169,6 +187,9 @@ struct AstInterpreter {
   }
 
   int interpret_variable_declaration(const VariableDeclaration &variable_declaration) {
+    // make sure the variable is not already declared
+    assert(variables.find(variable_declaration.name) == variables.end());
+    // FIXME: check type of initializer
     return variables[variable_declaration.name] = interpret(*variable_declaration.initializer);
   }
 
@@ -177,22 +198,35 @@ struct AstInterpreter {
   }
 
   int interpret_block(const Block &block) {
+    int result = 0;
     for (const auto &child : block.children) {
-      interpret(*child);
+      result = interpret(*child);
     }
-    return 0;
+    return result;
   }
 
-  int interpret_for(const For &for_loop) {
-    for (int i = interpret(*for_loop.initializer); interpret(*for_loop.condition); i++) {
-      interpret_block(*for_loop.body);
-      interpret(*for_loop.increment);
+  int interpret_while(const While &while_loop) {
+    int result = 0;
+    while (interpret(*while_loop.condition)) {
+      result = interpret_block(*while_loop.body);
     }
-    return 0;
+    return result;
   }
 
   int interpret_function_declaration(const FunctionDeclaration &function_declaration) {
     return interpret_block(*function_declaration.body);
+  }
+
+  int interpret_assignment(const Assignment &assignment) {
+    // make sure the variable is already declared
+    assert(variables.find(assignment.name) != variables.end());
+    // FIXME: check type of value
+    return variables[assignment.name] = interpret(*assignment.value);
+  }
+
+  int interpret_return(const Return &return_statement) {
+    // FIXME: check type of return value
+    return interpret(*return_statement.value);
   }
 
   int interpret(const Ast &ast) {
@@ -207,12 +241,16 @@ struct AstInterpreter {
         return interpret_variable_declaration(static_cast<const VariableDeclaration &>(ast));
       case Ast::Type::Increment:
         return interpret_increment(static_cast<const Increment &>(ast));
-      case Ast::Type::For:
-        return interpret_for(static_cast<const For &>(ast));
+      case Ast::Type::While:
+        return interpret_while(static_cast<const While &>(ast));
       case Ast::Type::Block:
         return interpret_block(static_cast<const Block &>(ast));
       case Ast::Type::FunctionDeclaration:
         return interpret_function_declaration(static_cast<const FunctionDeclaration &>(ast));
+      case Ast::Type::Assignment:
+        return interpret_assignment(static_cast<const Assignment &>(ast));
+      case Ast::Type::Return:
+        return interpret_return(static_cast<const Return &>(ast));
     }
     assert(false);
   }
--- main_ast.cpp
+++ main_ast.cpp
@@ -1,8 +1,11 @@
 // Example of code:
 //
 // fn void foo() {
+//   int j = 0;
 //   for(int i = 0; i < 10000000; i++) {
+//     j = i;
 //   }
+//   return j;
 // }
 
 // AST:
@@ -28,19 +31,26 @@ int main() {
   // begin function declaration
   auto function_decl =
       std::make_unique<FunctionDeclaration>("foo", ValueType::Void, std::make_unique<Block>());
-  // begin for loop
-  auto i = std::make_unique<VariableDeclaration>("i", ValueType::Int, std::make_unique<Literal>(0));
+
+  // declare j
+  function_decl->body->append<VariableDeclaration>("j", ValueType::Int,
+                                                   std::make_unique<Literal>(0));
+
+  // begin for loop (as a while loop)
+  function_decl->body->append<VariableDeclaration>("i", ValueType::Int,
+                                                   std::make_unique<Literal>(0));
   auto less_than =
-      std::make_unique<LessThan>(std::make_unique<Variable>("i"), std::make_unique<Literal>(10000000));
-  auto increment     = std::make_unique<Increment>(std::make_unique<Variable>("i"));
-  auto for_loop_body = std::make_unique<Block>();
-  auto for_loop = std::make_unique<For>(std::move(i), std::move(less_than), std::move(increment),
-                                        std::move(for_loop_body));
-  // end for loop
-  function_decl->body->children.push_back(std::move(for_loop));
+      std::make_unique<LessThan>(std::make_unique<Variable>("i"), std::make_unique<Literal>(1000));
+  auto while_loop_body = std::make_unique<Block>();
+  while_loop_body->append<Assignment>("j", std::make_unique<Variable>("i"));
+  while_loop_body->append<Increment>(std::make_unique<Variable>("i"));
+  auto while_loop = std::make_unique<While>(std::move(less_than), std::move(while_loop_body));
+  function_decl->body->children.push_back(std::move(while_loop));
+  // end while loop
+
+  function_decl->body->append<Return>(std::make_unique<Variable>("j"));
   // end function declaration
 
   function_decl->dump(std::cout);
-
   std::cout << AstInterpreter().interpret(*function_decl) << std::endl;
 }
